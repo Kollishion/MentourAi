02ef555ccbe19844af4e0a5ab2923dfae12ac003
Abhishek <abhishek321bose@gmail.com>
2026-07-25 14:26:34 +0530

    final almighty push

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
 
     return (
         <div className='z-4 flex fixed top-0 w-full items-start justify-between'>
-            <div className='pt-3.5 lg:pl-10'>
-                <div className='lg:w-28 w-15 text-3xl h-auto purple-fade-text'>	
+            <div className='pt-3.5 lg:pl-9'>
+                <div className='lg:w-40 w-15 text-3xl h-auto purple-fade-text'>	
 			MentourAi
                 </div>
             </div>
